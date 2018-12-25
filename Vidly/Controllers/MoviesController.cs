5d832fb67e67db33a2f5d0d959379651e5b6a88b
MasudHaider <masud6563@gmail.com>
2018-12-25 11:35:52 +0600

    Project initiation
    Initial Controllers, Models, ViewModels and necessary configs.

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek!" };
+            var customer = new List<Customer>
+            {
+                new Customer { Name = "Dave"},
+                new Customer { Name = "Nick"}
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customer
+            };
+
+            return View(viewModel);
+        } 
+    }
+}
