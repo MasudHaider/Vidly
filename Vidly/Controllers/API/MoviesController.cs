2ba17d83d4ff7c17279561bfbf898a626a89cad6
MasudHaider <masud6563@gmail.com>
2019-01-03 11:58:01 +0600

    delete movies and show movies using DataTables

--- Vidly/Controllers/API/MoviesController.cs
+++ Vidly/Controllers/API/MoviesController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -22,7 +23,7 @@ namespace Vidly.Controllers.API
         //GET api/Movies/
         public IEnumerable<MovieDto> GetMovies()
         {
-            return _Context.Movies.ToList().Select(Mapper.Map<Movie, MovieDto>);
+            return _Context.Movies.Include(m => m.Genre).ToList().Select(Mapper.Map<Movie, MovieDto>);
         }
 
         //GET api/Movies/1
--- Vidly/DTOs/MovieDto.cs
+++ Vidly/DTOs/MovieDto.cs
@@ -18,6 +18,8 @@ namespace Vidly.DTOs
         [Required]
         public byte GenreId { get; set; }
 
+        public GenreDto Genre { get; set; }
+
         public DateTime ReleaseDate { get; set; }
 
         public DateTime DateAdded { get; set; }
